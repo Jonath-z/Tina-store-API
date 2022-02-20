5bcc696c49303b36cfd590af9766a29dcadf489d
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-20 19:12:13 +0200

    get all products card route

---
+++ src/routes/getCardRoute.ts
@@ -0,0 +1,15 @@
+import {Router } from "express";
+import getCard from "../middleware/getCard";
+
+const path = '/card';
+const router = Router();
+
+router.route(path).post(getCard);
+
+const getCardRoute= {
+    path,
+    router
+}
+
+
+export default getCardRoute;
