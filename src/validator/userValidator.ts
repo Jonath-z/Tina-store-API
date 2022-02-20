e0b6e20f26e843f9826323845ef0ae27de76a746
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-20 03:05:11 +0200

    validator

--- src/validator/userValidator.ts
+++ src/validator/userValidator.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 import validatorHandler from "../middleware/validatorHandler";
 
 
-const signup = (req: Request, res: Response, next: NextFunction)=>{
+const signup = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body, 'from validator');
     const schema = Joi.object().keys({
         name: Joi.string().trim().min(4).required(),
         email: Joi.string().email().required(),
