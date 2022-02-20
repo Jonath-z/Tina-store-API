064e70292fb3b2b248e4f9fd46a8796e5820d1b2
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-20 19:16:30 +0200

    new routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,21 @@ import getProductsRoute from "./getProducts.route";
 import signupRoute from "./signupRoute";
 import loginRoute from "./loginRoute";
 import addToCardRoute from "./addToCardRoute";
+import getCardRoute from "./getCardRoute";
+import deleteProductByIdRoute from "./deleteProductByIdRoute";
+import archiveProductRoute from "./archiveProductRoute";
+import getArchivedProductRoute from "./getArchivedProductRoute";
 
 const routes: any[] = [
    uploadProductRoute,
    getProductsRoute,
    signupRoute,
    loginRoute,
-   addToCardRoute
-]
+   addToCardRoute,
+   getCardRoute,
+   deleteProductByIdRoute,
+   archiveProductRoute,
+   getArchivedProductRoute
+];
 
 export default routes;
