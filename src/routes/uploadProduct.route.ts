6b09529cc1f99ddc994d616f715418fe15c86082
Jonath-z <jonathanzihindula95@gmail.com>
2022-02-20 12:45:40 +0200

    product validator added as a middleware

--- src/routes/uploadProduct.route.ts
+++ src/routes/uploadProduct.route.ts
@@ -1,11 +1,16 @@
 import {Router } from "express";
 import uploadProducts from "../middleware/uploadProduct";
+import productValidator from "../validator/productValidator";
 
 
 const path = '/upload/products';
 const router = Router();
 
-router.route(path).post(uploadProducts)
+router.route(path)
+    .post(
+        productValidator,
+        uploadProducts
+    )
 
 
 const uploadProductRoute = {
